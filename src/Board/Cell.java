a0d56d234ec385afcae76098c1f8f787b723175d
danbuis <dan.buis88@gmail.com>
2017-04-14 16:37:58 -0400

    First Commit

---
+++ src/Board/Cell.java
@@ -0,0 +1,14 @@
+package Board;
+
+
+/**
+ * This cell is one of 81 units on the whole board.  It keeps track of what possible numbers can still go in it, and
+ * has the ability to look at groups of other cells to find out what options they might still have.
+ * @author dbuis
+ *
+ */
+public class Cell {
+	
+	
+
+}
